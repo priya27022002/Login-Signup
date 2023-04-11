68c476d7bd4d8d0a3cd2ef96afe0d293b7de40ff
RAJAPRIYAH <priya@priyajagatheeswaran2002@gmail.com>
2023-04-11 21:09:27 +0530

    First commit

---
+++ src/mongodb.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/LoginFormPractice")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection=new mongoose.model('Collection1',logInSchema)
+
+module.exports=collection
